2d5daa8d06b3d4892941cee87d727d8649068121
mdefab <defab@live.ca>
2022-11-17 17:16:43 -0500

    Add api

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd Party
+    'rest_framework',
     # Local
     'books.apps.BooksConfig',
+    'apis.apps.ApisConfig',
 ]
 
 MIDDLEWARE = [
